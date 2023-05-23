3a2e34abbd6cdfb19952e9da4af941feaf4a5dc3
rodrigo.jdasilva <rodrigo.jdasilva@mv.com.br>
2023-05-23 14:38:05 -0300

    Add style

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 function searchAndReplace() {
-  let originalText = document.getElementById("originalText").value;
-  let parameters = document.getElementById("replaceText").value;
+  let originalText = document.getElementById("floatingTextarea").value;
+  let parameters = document.getElementById("floatingTextarea3").value;
 
   let elementsArray = parameters.split(",").map(function (element) {
     return element.trim();
   });
   let modifiedText = sqlFormatter.format(originalText, {
-    dialect: "plsql",
     params: elementsArray,
     keywordCase: "upper",
     logicalOperatorNewline: "before",
-    identStyle: "tabularRight",
   });
 
-  document.getElementById("modifiedText").value = modifiedText;
+  document.getElementById("floatingTextarea2").value = modifiedText;
 }
